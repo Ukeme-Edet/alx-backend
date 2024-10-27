2cb2154c540de7edc092ab45ceb72d94899aa3ad
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-27 23:42:34 +0100

    Refactor pagination function to handle out-of-range page numbers and improve type checking

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -36,18 +36,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Get the page of the dataset
-
-        Args:
-            page: int
-            page_size: int
-
-        Returns:
-            List[List]
-        """
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-
+        """Retrieves a page of data."""
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
-        return self.dataset()[start:end]
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
